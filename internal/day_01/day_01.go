45b7a7749509ab9e587605f8c50585931901e7aa
jcardenasc93 <jcardenasc93@gmail.com>
2025-12-06 01:49:05 -0500

    Solves day1 part 2

--- internal/day_01/day_01.go
+++ internal/day_01/day_01.go
@@ -4,6 +4,7 @@ import (
 	"aoc/aoc_2025/pkg/utils"
 	"fmt"
 	"log"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -16,6 +17,8 @@ func Solve() {
 	fmt.Println("Solving part 1...")
 	solvePart1()
 	fmt.Println()
+	fmt.Println("Solving part 2...")
+	solvePart2()
 }
 func solvePart1() {
 	position := 50
@@ -33,6 +36,31 @@ func solvePart1() {
 
 	fmt.Printf("The password is: %d\n", password)
 
+}
+
+func solvePart2() {
+	position := 50
+	password := 0
+	rotations := utils.ReadInputFile("01")
+
+	for _, rot := range rotations {
+		val := parseRotation(rot)
+		nextPos := position + val
+		zeroCross := 0
+		if nextPos > position {
+			end := math.Floor(float64(nextPos) / 100.0)
+			start := math.Floor(float64(position) / 100.0)
+			zeroCross = int(end - start)
+		} else {
+			start := math.Floor(float64(position-1) / 100.0)
+			end := math.Floor(float64(nextPos-1) / 100.0)
+			zeroCross = int(start - end)
+
+		}
+		password += zeroCross
+		position = nextPos
+	}
+	fmt.Printf("The password is: %d\n", password)
 }
 
 func parseRotation(rotation string) int {
